cf6b98521f89655ef67c60d843422c1b502dd960
sridhar <sridharalapati22@gmail.com>
2020-12-10 19:53:48 +0530

    Updates

---
+++ precache-manifest.44002bc240cfa123c37d288da5b0a1d8.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60701637d5937e69a93e4d94dd3e310",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/index.html"
+  },
+  {
+    "revision": "b253acd205f47f2d69ef",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/css/main.03019f08.chunk.css"
+  },
+  {
+    "revision": "82355a81391b9a4884b7",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/js/2.36cfe30e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/js/2.36cfe30e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b253acd205f47f2d69ef",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/js/main.10918830.chunk.js"
+  },
+  {
+    "revision": "4ab7cc379a2ed1830a10",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/js/runtime-main.c5f6e30c.js"
+  },
+  {
+    "revision": "3d1963527e3ae3dcdf199e3975e6617e",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/Bolo.3d196352.jpg"
+  },
+  {
+    "revision": "4bfa26ce037b103614740f503d278abb",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/Full stack.4bfa26ce.jpg"
+  },
+  {
+    "revision": "f3d7014bd47b70d9938f6d88ba0087a8",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/IRSC.f3d7014b.jpg"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "fc5854e53119b5cda05cf429e5760eb8",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/c.fc5854e5.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "13a096531e50277a3fb17460c39db221",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/java.13a09653.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "6ede34dbde625eb0306528f0936e33d0",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/logo.6ede34db.png"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "e39f29aa64946133daaae75c3b988f4d",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/p_background.e39f29aa.jpg"
+  },
+  {
+    "revision": "ce5a416446d78554143fb842ea6e5aa0",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/profile.ce5a4164.jpg"
+  },
+  {
+    "revision": "146716bdf1a351db5705ec1e6a25ed08",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/python.146716bd.svg"
+  },
+  {
+    "revision": "ef194ddf5faf63cb6cc580a4535be695",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/react-bootstrap.ef194ddf.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "8cab1606de2132e6d354a7fa73b5f90d",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/slide1.8cab1606.jpg"
+  },
+  {
+    "revision": "e6b5de8e170a2e8acaec98979d01feb6",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/slide2.e6b5de8e.jpg"
+  },
+  {
+    "revision": "24f184bf3e93fcc4a812d5d4bef873fe",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/slide3.24f184bf.jpg"
+  },
+  {
+    "revision": "281c5f6fb40198ba75f037b49a5e6789",
+    "url": "/Sridhar-Reddy-Alapati-portfolio/static/media/zersesy Infotech.281c5f6f.jpg"
+  }
+]);
